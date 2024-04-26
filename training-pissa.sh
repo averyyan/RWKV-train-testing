10e157c00a86fe64fe375d88009d48a7bb5a187f
averyyan2010 <305482686@qq.com>
2024-04-26 09:05:39 +0000

    pissa trainning

--- training-pissa.sh
+++ training-pissa.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
+BASE_MODEL="models/RWKV-x060-World-3B-v2.1-20240417-ctx4096.pth"
 MODEL_TYPE="x060"
-N_LAYER="12"
-N_EMBD="768"
-CTX_LEN="512" 
-PROJ_DIR="out/L"$N_LAYER"-D"$N_EMBD"-"$MODEL_TYPE"-"$CTX_LEN"-pissa"
-M_BSZ="16"
-LR_INIT="5e-4"
-LR_FINAL="5e-5"
+N_LAYER="32"
+N_EMBD="2560"
+CTX_LEN="4096"
+QUANT="nf4"
+M_BSZ="1"
+
+LR_INIT="1e-4"
+LR_FINAL="1e-5"
 GRAD_CP=1
-EPOCH_SAVE=10 
+EPOCH_SAVE=1
 N_NODE=1 
 GPU_PER_NODE=1 
 DS_BUCKET_MB=2
 
-python3 train.py --proj_dir $PROJ_DIR --data_file "data/minipile" --my_exit_tokens 1498226207 --magic_prime 2926181 \
+
+PROJ_DIR="out/L"$N_LAYER"-D"$N_EMBD"-"$MODEL_TYPE"-"$CTX_LEN"-pissa"-$QUANT
+
+python3 train.py --load_model $BASE_MODEL --proj_dir $PROJ_DIR --data_file "data/data" \
     --data_type binidx --vocab_size 65536 \
     --ctx_len $CTX_LEN --epoch_steps 1000 --epoch_count 20 --epoch_begin 0 --epoch_save $EPOCH_SAVE --micro_bsz $M_BSZ \
     --n_layer $N_LAYER --n_embd $N_EMBD \
@@ -23,4 +28,4 @@ python3 train.py --proj_dir $PROJ_DIR --data_file "data/minipile" --my_exit_toke
     --my_testing "x060" \
     --lora_load rwkv-0 --lora --lora_r 64 --lora_alpha 128 --lora_dropout 0.01 --lora_parts=att,ffn,time,ln \
     --PISSA --svd_niter 4 \
-    --quant nf4
+    --quant $QUANT
